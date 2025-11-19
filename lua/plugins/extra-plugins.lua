edf2adc41c42e6abc8d82700a3e32d09daa33a49
Alec Lucas <alec@kovocredit.com>
2025-11-19 09:45:42 -0500

    add multicursors plugin with keybindings

--- lua/plugins/extra-plugins.lua
+++ lua/plugins/extra-plugins.lua
@@ -7,4 +7,13 @@ return {
   { 'chrisgrieser/nvim-spider' },
   { 'danielfalk/smart-open.nvim', branch = '0.2.x' },
   { 'mbbill/undotree', cmd = 'UndotreeToggle' },
+  {
+    'smoka7/multicursors.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'smoka7/hydra.nvim',
+    },
+    opts = {},
+  },
 }
